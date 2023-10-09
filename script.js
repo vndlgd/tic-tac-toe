fd3dd7d61146d4e7a58702e2db243579d81fcdb1
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2023-10-09 15:37:48 -0700

    Initialize gameboard with empty cells

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 // gameboard module because we only need one of it
 const gameBoard = (function () {
     // store gameboard as an array 
-    const gameboard = [['X', 'O', 'X'], ['O', 'X', 'O'], ['X', 'O', 'X']];
+    const gameboard = [];
+    const rows = 3;
+    const cols = 3;
 
+    // fill game board with arrays that contain an object that is an empty cell 
+    for (let i = 0; i < rows; i++) {
+        gameboard.push([]);
+        for (let j = 0; j < cols; j++) {
+            gameboard[i].push(Cell());
+        }
+    }
     return {
         gameboard
     };
 })();
+
+// Create empty cells 
+function Cell() {
+    let cell = "";
+    return cell;
+}
 
 // displayController module because we only need one of it
 const displayController = (function () {
